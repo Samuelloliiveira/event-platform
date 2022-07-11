414f6bf28139592e9e535b6c9ec2de9cf133701b
Samuel Oliveira <samuelcontatodev@gmail.com>
2022-07-11 20:37:28 -0300

    criando página de inscrição

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        blur: 'url(/src/assets/blur-background.jpeg)'
+      },
       fontFamily: {
         sans: 'Roboto, sans-serif'
       },
